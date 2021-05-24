85bb4dba3ba109d5774fdce9a3f7e894f84a44a7
huizhuansam <huizhuansam@users.noreply.github.com>
2021-05-25 00:06:58 +0800

    add sign out button and functionality

--- src/components/Navigations/NavigationBarWeb.vue
+++ src/components/Navigations/NavigationBarWeb.vue
@@ -11,19 +11,37 @@
 
     <AuthenticationButton :action="'Login'" v-if="$route.name === 'Home'" />
     <AuthenticationButton :action="'SignUp'" v-if="$route.name === 'Home'" />
+    <v-btn v-else light @click="userSignout">
+      Sign Out
+    </v-btn>
   </v-app-bar>
 </template>
 
 <script lang="ts">
 import AuthenticationButton from '@/components/Authentication/AuthenticationButton.vue';
 import LogoSponsorr from '@/components/BuildingElements/LogoSponsorr.vue';
+import useAuth from '@/composable/authComposition';
+import { defineComponent } from '@vue/composition-api';
 
-export default {
+export default defineComponent({
+  name: 'NavigationBarWeb',
   components: {
     AuthenticationButton,
     LogoSponsorr,
   },
-};
+  setup(_, { root }) {
+    const { signout } = useAuth();
+
+    const userSignout = () => {
+      signout();
+      root.$router.push({
+        name: 'Home',
+      });
+    };
+
+    return { userSignout };
+  },
+});
 </script>
 
 <style scoped>
