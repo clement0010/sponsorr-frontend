0f94e5ee6bd77cb2e53c9b0d8ce5800e0eff8749
huizhuansam <huizhuansam@users.noreply.github.com>
2021-05-25 17:35:43 +0800

    make footer responsive

--- src/components/PageComponents/Footer.vue
+++ src/components/PageComponents/Footer.vue
@@ -1,17 +1,29 @@
 <template>
-  <v-footer light padless height="75" class="pa-4">
-    <span>NUS Orbital 2021</span>
-
-    <v-spacer></v-spacer>
-
-    <span> &copy; {{ new Date().getFullYear() }} Sponsorr! </span>
-
-    <v-spacer></v-spacer>
-
-    <v-btn class="mx-4" light icon>
-      <v-icon size="24px" color="black">
-        mdi-github
-      </v-icon>
-    </v-btn>
+  <v-footer absolute light>
+    <v-container fill-height>
+      <v-row align="center" justify="center">
+        <v-col cols="auto">
+          NUS Orbital 2021
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col cols="auto">
+          &copy; {{ new Date().getFullYear() }} Sponsorr!
+        </v-col>
+        <v-spacer></v-spacer>
+        <v-col cols="auto">
+          <v-btn class="mx-4" light icon>
+            <v-icon size="30px" color="black">
+              mdi-github
+            </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-footer>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1320px;
+}
+</style>
