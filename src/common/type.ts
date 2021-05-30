0bdef4bcf6bd26c68ff7ff2587f3cfbb28dfe4dc
Clement Tee <clement@usefriz.com>
2021-05-30 21:12:53 +0800

    feat: add new type for firebase update

---
+++ src/common/type.ts
@@ -0,0 +1,24 @@
+type PathImpl<T, K extends keyof T> =
+  K extends string
+  ? T[K] extends Record<string, any>
+  ? T[K] extends ArrayLike<any>
+  ? K | `${K}.${PathImpl<T[K], Exclude<keyof T[K], keyof any[]>>}`
+  : K | `${K}.${PathImpl<T[K], keyof T[K]>}`
+  : K
+  : never
+type Path<T> = PathImpl<T, keyof T> | keyof T
+type PathValue<T, P extends Path<T>> =
+  P extends `${infer K}.${infer Rest}`
+  ? K extends keyof T
+  ? Rest extends Path<T[K]>
+  ? PathValue<T[K], Rest>
+  : never
+  : never
+  : P extends keyof T
+  ? T[P]
+  : never
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type UpdateData<T extends object> = Partial<{
+  [TKey in Path<T>]: PathValue<T, TKey>
+}>
