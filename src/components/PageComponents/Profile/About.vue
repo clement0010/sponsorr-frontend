ec23b46b330d990cdf235e6fe43da83e16b7a6be
Clement Tee <clement@usefriz.com>
2021-05-30 21:14:18 +0800

    feat: add edit about profile

--- src/components/PageComponents/Profile/About.vue
+++ src/components/PageComponents/Profile/About.vue
@@ -1,18 +1,31 @@
 <template>
-  <v-card-text class="black--text text-body-1">
-    {{ about }}
-  </v-card-text>
+  <v-container>
+    <v-card-title class="text-h4 black--text">
+      About
+      <EditAbout
+        :about="about"
+        @edit-about="payload => $emit('edit', payload)"
+      />
+    </v-card-title>
+    <v-card-text class="black--text text-body-1">
+      {{ about }}
+    </v-card-text>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
+import EditAbout from '@/components/UserActions/EditAbout.vue';
 
 export default defineComponent({
+  components: {
+    EditAbout,
+  },
   props: {
     about: {
       type: String,
       required: true,
-      default: 'Test',
+      default: 'Empty...',
     },
   },
 });
