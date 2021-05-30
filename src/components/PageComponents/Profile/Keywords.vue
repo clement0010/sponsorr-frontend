b0edcbe4bf8a2531c26e48fa23c1cbdbaf9038fb
Clement Tee <clement@usefriz.com>
2021-05-31 00:15:52 +0800

    feat: add edit keywords

--- src/components/PageComponents/Profile/Keywords.vue
+++ src/components/PageComponents/Profile/Keywords.vue
@@ -1,27 +1,44 @@
 <template>
-  <v-card-text>
-    <v-chip-group column>
-      <v-chip
-        v-for="keyword in keywords"
-        :key="keyword"
-        disabled
-      >
-        {{ keyword }}
-      </v-chip>
-    </v-chip-group>
-  </v-card-text>
+  <v-card
+    color="transparent"
+    flat
+  >
+    <v-card-title class="text-h4 black--text">
+      Keywords
+      <EditKeywords
+        :keywords="keywords"
+        @edit-keywords="payload => $emit('edit', payload)"
+      />
+    </v-card-title>
+    <v-card-text>
+      <v-chip-group column>
+        <v-chip
+          v-for="keyword in keywords"
+          :key="keyword"
+          disabled
+        >
+          {{ keyword }}
+        </v-chip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script lang="ts">
-import { PropType } from 'vue';
+import { defineComponent } from '@vue/composition-api';
+import EditKeywords from '@/components/UserActions/EditKeywords.vue';
 
-export default {
+export default defineComponent({
+  name: 'ProfileKeywords',
+  components: {
+    EditKeywords,
+  },
   props: {
     keywords: {
-      type: Array as PropType<string[]>,
+      type: Array as () => string[],
       required: true,
       default: (): string[] => ['Default value'],
     },
   },
-};
+});
 </script>
--- src/components/UserActions/EditKeywords.vue
+++ src/components/UserActions/EditKeywords.vue
@@ -1,7 +1,15 @@
 <template>
-  <v-dialog v-model="dialog" max-width="600px">
+  <v-dialog
+    v-model="dialog"
+    max-width="600px"
+  >
     <template v-slot:activator="{ on, attrs }">
-      <v-btn icon class="mx-5" v-bind="attrs" v-on="on">
+      <v-btn
+        icon
+        class="mx-5"
+        v-bind="attrs"
+        v-on="on"
+      >
         <v-icon color="black">
           mdi-pencil
         </v-icon>
@@ -17,13 +25,46 @@
         </v-card-title>
 
         <v-card-text>
+          <v-chip-group
+            column
+            class="px-4"
+          >
+            <v-chip
+              v-for="(keyword, index) in input"
+              :key="keyword"
+              close
+              @click:close="removeKeyword(index)"
+            >
+              {{ keyword }}
+            </v-chip>
+          </v-chip-group>
+
+          <v-text-field
+            v-model="keywordInput"
+            class="px-4"
+            dense
+            outlined
+            label="Type here and hit 'enter' to save"
+            @keydown.enter="addKeyword"
+          />
         </v-card-text>
+
         <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn class="error" rounded text @click="cancel">
+          <v-spacer />
+          <v-btn
+            class="error"
+            rounded
+            text
+            @click="cancel"
+          >
             Cancel
           </v-btn>
-          <v-btn class="success" rounded text @click="save">
+          <v-btn
+            class="success"
+            rounded
+            text
+            @click="edit"
+          >
             Save
           </v-btn>
         </v-card-actions>
@@ -32,33 +73,55 @@
   </v-dialog>
 </template>
 
-<script>
-import { defineComponent, ref } from '@vue/composition-api';
-import { abort, payload, send } from '@/utils/profile';
-import { validEmailRule, validURLRule } from '@/utils/validation';
+<script lang="ts">
+import {
+  defineComponent, ref, toRefs,
+} from '@vue/composition-api';
 
 export default defineComponent({
-  setup() {
+  props: {
+    keywords: {
+      type: Array as () => string[],
+      required: true,
+    },
+  },
+  setup(props, { emit }) {
+    const { keywords } = toRefs(props);
+    const input = keywords.value;
+
+    const keywordInput = ref('');
     const dialog = ref(false); // Dialog is closed by default
 
     const cancel = () => {
       dialog.value = false; // Closes dialog
-      abort();
     };
 
-    const save = () => {
+    const edit = () => {
       dialog.value = false; // Closes dialog
-      send();
+      emit('edit-keywords', {
+        keywords: input,
+      });
+    };
+
+    const removeKeyword = (index: number): void => {
+      input.splice(index, 1);
+    };
+
+    const addKeyword = (e:Event): void => {
+      e.preventDefault();
+
+      input.push(keywordInput.value);
+      keywordInput.value = '';
     };
 
     return {
       dialog,
       cancel,
-      save,
-      payload,
-      send,
-      validEmailRule,
-      validURLRule,
+      edit,
+      input,
+      removeKeyword,
+      addKeyword,
+      keywordInput,
     };
   },
 });
