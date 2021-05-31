905557eb01586ddf5692ebbda2269e217cb273b2
huizhuansam <huizhuansam@users.noreply.github.com>
2021-05-31 18:11:44 +0800

    feat: create snackbar component

---
+++ src/components/BuildingElements/Snackbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-snackbar
+    v-model="snackbar"
+    timeout="2000"
+    :color="status"
+  >
+    {{ message }}
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'Snackbar',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['error', 'success'],
+    },
+  },
+  setup() {
+    const snackbar = ref(true);
+    return { snackbar };
+  },
+});
+</script>
---
+++ src/views/Playground.vue
@@ -0,0 +1,23 @@
+<template>
+  <BasePage>
+    <Snackbar
+      :message="'HowdyHoo'"
+      :status="'error'"
+    />
+  </BasePage>
+</template>
+
+<script lang="ts">
+import BasePage from '@/layouts/BasePage.vue';
+import Snackbar from '@/components/BuildingElements/Snackbar.vue';
+
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'Playground',
+  components: {
+    BasePage,
+    Snackbar,
+  },
+});
+</script>
