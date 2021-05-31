b2bc51268d57f3dedec784f7dce15102a7320be5
Clement Tee <clement@usefriz.com>
2021-06-01 05:36:30 +0800

    style: modify prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/recommended',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ["plugin:vue/recommended", "@vue/airbnb", "@vue/typescript/recommended"],
   parserOptions: {
     ecmaVersion: 2020,
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "import/prefer-default-export": "off",
   },
 };
