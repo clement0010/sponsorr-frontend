7a1f6e86c4a9e24a20922a0e9e5fbc307ca09e24
huizhuansam <huizhuansam@users.noreply.github.com>
2021-06-01 21:29:13 +0800

    fix: disable dark theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -24,8 +24,26 @@ const opts = {
         success: '#4CAF50',
         warning: '#FFC107',
       },
+      light: {
+        anchor: '#1B1852',
+        primary: '#1B1852',
+        secondary: '#E4E4E4',
+        accent1: '#FF9131',
+        accent2: '#0059A3',
+        accent3: '#FFC981',
+        white: '#FFFFFF',
+        balck: '#000000',
+        // Config here
+        // primary: '#1976D2',
+        // secondary: '#424242',
+        // accent: '#82B1FF',
+        error: '#FF5252',
+        // info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107',
+      },
     },
-    dark: true,
+    dark: false,
   },
 };
 
