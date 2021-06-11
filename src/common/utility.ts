31544175398f092aee4dd2e7dcc02c39e848d99a
huizhuansam <huizhuansam@users.noreply.github.com>
2021-06-11 15:39:14 +0800

    feat: add more dayjs utility functions

--- src/common/utility.ts
+++ src/common/utility.ts
@@ -1,24 +1,40 @@
 import dayjs from 'dayjs';
 
 /**
- * Generate date with the specified format
+ * Generate today's date with the specified format
  *
  * default value: DD/MM/YYYY
  */
-export const generateDateFormat = (format?: string): string => {
+export const generateTodayDateFormat = (format?: string): string => {
   return dayjs().format(format || 'DD/MM/YYYY');
 };
 
+/**
+ * Generate current unix timestamp
+ */
 export const generateCurrentUnixTime = (): number => {
   return dayjs().unix();
 };
 
+/**
+ * Generate unix timestamp with given date
+ */
 export const generateUnixTimeFromDate = (date: string): number => {
   return dayjs(date).unix();
 };
 
+/**
+ * Generate date with specified format given unix timestamp
+ */
 export const generateDateFromUnixTime = (unixTime: number): string => {
   return dayjs.unix(unixTime).format('DD/MM/YYYY');
+};
+
+/**
+ * Generate range of dates given array of unix timestamps
+ */
+export const generateDateRangeFromUnixTimeRange = (unixTimeRange: number[]): string => {
+  return unixTimeRange.map((unixTime) => generateDateFromUnixTime(unixTime)).join(' - ');
 };
 
 /**
