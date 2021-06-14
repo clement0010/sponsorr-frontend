01d39df9c3d0cb0f73406cf3ecea9d286d84bd05
Clement Tee <clement@usefriz.com>
2021-06-15 05:02:03 +0800

    feat: integrate fetch event logic

--- src/composable/eventComposition.ts
+++ src/composable/eventComposition.ts
@@ -1,159 +1,61 @@
 import { EventCategory, SponsorEvent } from '@/types/index';
 import { computed, ref } from '@vue/composition-api';
-import { isPastEvent } from '@/common/utility';
-import database from './mockFirestore';
+import { isPastEvent } from '@/common/utils';
+import { getUserEventByStatusFromDb } from '@/common/firestore/dashboard';
+import { draftsCategory, pastCategory, upcomingCategory } from '@/common/dashboardConfig';
+import { EventGroup } from '@/types/enum';
 
 // eslint-disable-next-line
 export default function useEvent() {
-  const {
-    fetchUpcomingEvents,
-    fetchPastEvents,
-    fetchDrafts,
-    removeEvent,
-    publishStatus,
-    create,
-  } = database();
-
   const loading = ref(true);
 
   const eventCategories = ref<EventCategory[]>();
 
-  const upcomingCategory: EventCategory = {
-    name: 'Upcoming Events',
-    loaded: false,
-    headers: [
-      {
-        text: 'Event Title',
-        value: 'title',
-      },
-      {
-        text: 'Date',
-        value: 'date',
-      },
-      {
-        text: 'Venue',
-        value: 'venue',
-      },
-      {
-        text: 'Views',
-        value: 'views',
-        align: 'center',
-      },
-      {
-        text: 'Clicks',
-        value: 'clicks',
-        align: 'center',
-      },
-      {
-        text: 'Matches',
-        value: 'matches',
-        align: 'center',
-      },
-      {
-        text: 'Actions',
-        value: 'actions',
-        sortable: false,
-        align: 'center',
-      },
-    ],
-    contents: [],
-    fallback: 'No upcoming events',
-  };
-
-  const pastCategory: EventCategory = {
-    name: 'Past Events',
-    loaded: false,
-    headers: [
-      {
-        text: 'Event Title',
-        value: 'title',
-      },
-      {
-        text: 'Date',
-        value: 'date',
-      },
-      {
-        text: 'Venue',
-        value: 'venue',
-      },
-      {
-        text: 'Actions',
-        value: 'actions',
-        sortable: false,
-        align: 'center',
-      },
-    ],
-    contents: [],
-    fallback: 'No past events',
-  };
-
-  const draftsCategory: EventCategory = {
-    name: 'Drafts',
-    loaded: false,
-    headers: [
-      {
-        text: 'Event Title',
-        value: 'title',
-      },
-      {
-        text: 'Date',
-        value: 'date',
-      },
-      {
-        text: 'Venue',
-        value: 'venue',
-      },
-      {
-        text: 'Actions',
-        value: 'actions',
-        sortable: false,
-        align: 'center',
-      },
-    ],
-    contents: [],
-    fallback: 'Nothing in drafts',
-  };
-
   const initialise = async (): Promise<void> => {
-    eventCategories.value = [];
-    eventCategories.value.push(upcomingCategory);
-    eventCategories.value.push(pastCategory);
-    eventCategories.value.push(draftsCategory);
-    loading.value = true;
-    await fetchUpcomingEvents().then((upcoming) => {
-      upcomingCategory.contents = upcoming;
+    try {
+      loading.value = true;
+      eventCategories.value = [upcomingCategory, pastCategory, draftsCategory];
+
+      // Todo: Integrate UID
+      upcomingCategory.contents = await getUserEventByStatusFromDb(
+        'M2vz9kjWxZZfRe3eBocZ3ktSYcX2',
+        EventGroup.Upcoming,
+      );
       upcomingCategory.loaded = true;
-    });
-    loading.value = false;
+    } catch (err) {
+      console.error(err);
+      throw new Error(err);
+    } finally {
+      loading.value = false;
+    }
   };
 
   const fetchEvents = async (eventCategory: EventCategory): Promise<void> => {
-    if (!eventCategory.loaded) {
-      loading.value = true;
-      const categoryRef = eventCategory;
-      switch (eventCategory.name) {
-        case 'Upcoming Events':
-          categoryRef.contents = await fetchUpcomingEvents();
-          break;
-        case 'Past Events':
-          categoryRef.contents = await fetchPastEvents();
-          break;
-        default:
-          categoryRef.contents = await fetchDrafts();
-          break;
+    try {
+      if (!eventCategory.loaded) {
+        loading.value = true;
+        const categoryRef = eventCategory;
+
+        // Todo: Integrate UID
+        categoryRef.contents = await getUserEventByStatusFromDb(
+          'M2vz9kjWxZZfRe3eBocZ3ktSYcX2',
+          eventCategory.name,
+        );
+        categoryRef.loaded = true;
       }
-      categoryRef.loaded = true;
+    } catch (err) {
+      console.error(err);
+      throw new Error(err);
+    } finally {
       loading.value = false;
     }
   };
 
   const deleteEvent = async (event: SponsorEvent, eventCategory: EventCategory): Promise<void> => {
-    await removeEvent(event);
     eventCategory.contents.splice(eventCategory.contents.indexOf(event), 1);
   };
 
   const publishEvent = async (event: SponsorEvent) => {
-    await publishStatus(event, true);
     draftsCategory.contents.splice(draftsCategory.contents.indexOf(event), 1);
     if (isPastEvent(event.date[0])) {
       pastCategory.contents.push(event);
@@ -163,7 +65,6 @@ export default function useEvent() {
   };
 
   const unpublishEvent = async (event: SponsorEvent): Promise<void> => {
-    await publishStatus(event, false);
     if (isPastEvent(event.date[0])) {
       pastCategory.contents.splice(pastCategory.contents.indexOf(event), 1);
     } else {
@@ -173,7 +74,7 @@ export default function useEvent() {
   };
 
   const createEvent = async (event: SponsorEvent): Promise<void> => {
-    await create(event);
+    console.log(event);
   };
 
   return {
