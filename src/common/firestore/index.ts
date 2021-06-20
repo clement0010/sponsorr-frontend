b9fd4988bc3da85b7abca9c0bb0f7d86b3d5c34b
Clement Tee <clement@usefriz.com>
2021-06-21 02:15:21 +0800

    feat: add matches and events collections

--- src/common/firestore/index.ts
+++ src/common/firestore/index.ts
@@ -1,3 +1,5 @@
 export * from './dashboard';
 export * from './profile';
 export * from './utils';
+export * from './event';
+export * from './matches';
--- src/common/firestore/utils.ts
+++ src/common/firestore/utils.ts
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 
 import { firestore } from '@/common/firebase';
 import { Profile, SponsorEvent } from '@/types';
+import { MatchedJunction } from '../type';
 
 // eslint-disable-next-line
 export const converter = <T>() => ({
@@ -16,6 +17,6 @@ export const dataPoint = <T>(collectionPath: string) =>
 export const db = {
   // list your collections here
   profile: dataPoint<Profile>('users'),
-  // eslint-disable-next-line
-  events: (uid: string) => dataPoint<SponsorEvent>(`users/${uid}/events`),
+  matches: dataPoint<MatchedJunction>('matches'),
+  events: dataPoint<SponsorEvent>(`events`),
 };
