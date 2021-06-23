43f73f09a0bf6254516646a312b7b83bdeffdfd9
huizhuansam <huizhuansam@users.noreply.github.com>
2021-06-24 03:00:16 +0800

    feat: improve 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,8 +1,21 @@
 <template>
   <BasePage>
-    <v-row justify="center">
-      <v-col align="center"> Why are you here? </v-col>
-    </v-row>
+    <v-container fluid class="ma-0 pa-0">
+      <v-img src="@/assets/image-404.jpeg" height="100vh">
+        <v-row align="center" justify="center">
+          <v-col>
+            <v-card flat color="transparent">
+              <v-card-title class="justify-center text-h1 white--text">
+                ERROR 404
+              </v-card-title>
+              <v-card-title class="justify-center text-h4 white--text">
+                Look somewhere else!
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-img>
+    </v-container>
   </BasePage>
 </template>
 
