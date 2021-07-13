36c7c1918f4db746b3bfb32b2405409cd59063de
Hui Zhuan <huizhuansam@hotmail.com>
2021-07-13 14:20:48 +0800

    feat: error handling

--- src/composable/analyticsComposition.ts
+++ src/composable/analyticsComposition.ts
@@ -3,9 +3,12 @@ import { getUserEventFromDb } from '@/common/firestore/dashboard';
 import { SponsorEvents, Role, Matches, MatchStatus } from '@/types';
 import { onMounted, ref, computed } from '@vue/composition-api';
 import { uid, role } from './store';
+import useSnackbar from './snackbarComposition';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function useAnalytics() {
+  const { alert } = useSnackbar();
+
   const data = ref<number[]>([]);
 
   const summarizeEvents = (events: SponsorEvents, value: string) => {
@@ -30,17 +33,22 @@ export default function useAnalytics() {
   };
 
   const initialise = async (userId: string, userRole: Role | undefined) => {
-    if (userRole === 'EventOrganiser') {
-      const events = await getUserEventFromDb(userId);
-      data.value.push(summarizeEvents(events, 'clicks'));
-      data.value.push(summarizeEvents(events, 'views'));
-      data.value.push(summarizeEvents(events, 'matches'));
-    }
-    if (userRole === 'Sponsor') {
-      const matches = await getAllMatchedEventFromDb(userId);
-      data.value.push(summarizeMatches(matches, 'accepted'));
-      data.value.push(summarizeMatches(matches, 'pending'));
-      data.value.push(summarizeMatches(matches, 'rejected'));
+    try {
+      if (userRole === 'EventOrganiser') {
+        const events = await getUserEventFromDb(userId);
+        data.value.push(summarizeEvents(events, 'clicks'));
+        data.value.push(summarizeEvents(events, 'views'));
+        data.value.push(summarizeEvents(events, 'matches'));
+      }
+      if (userRole === 'Sponsor') {
+        const matches = await getAllMatchedEventFromDb(userId);
+        data.value.push(summarizeMatches(matches, 'accepted'));
+        data.value.push(summarizeMatches(matches, 'pending'));
+        data.value.push(summarizeMatches(matches, 'rejected'));
+      }
+    } catch (err) {
+      console.error(err);
+      alert('There was an issue.');
     }
   };
 
