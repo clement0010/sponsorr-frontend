4a14fb242c934f0a9ab2e8634b5da3c36f408ae3
Hui Zhuan <huizhuansam@hotmail.com>
2021-07-23 22:58:42 +0800

    feat: add verification modal

---
+++ src/components/Verification/VerificationModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-overlay :value="!emailVerified" absolute>
+    <v-card class="white pa-1" max-width="500">
+      <v-card-title class="black--text">
+        Email Verification Needed
+      </v-card-title>
+      <v-card-text class="black--text">
+        Welcome to Sponsorr!
+        <br />
+        <br />
+        We noticed that you have not verified your account. To continue using the application,
+        please head to the email address that you've used to register with us and click on the
+        verification link.
+      </v-card-text>
+    </v-card>
+  </v-overlay>
+</template>
+
+<script lang="ts">
+import { emailVerified } from '@/composable/store';
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'VerificationModal',
+  setup() {
+    return {
+      emailVerified,
+    };
+  },
+});
+</script>
--- src/layouts/BasePage.vue
+++ src/layouts/BasePage.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container fluid class="primary pa-0 pb-15">
+    <VerificationModal />
     <slot />
   </v-container>
 </template>
 
 <script lang="ts">
+import VerificationModal from '@/components/Verification/VerificationModal.vue';
+
 import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
   name: 'BasePage',
+  components: {
+    VerificationModal,
+  },
 });
 </script>
